b32f80128d188198c5537743d40cfb027c366be7
Srujan <62290482+sj10111@users.noreply.github.com>
2022-07-11 11:13:46 +0530

    Update ShowData.js

--- pages/ShowData.js
+++ pages/ShowData.js
@@ -19,7 +19,7 @@ function ShowData(props) {
             <div className="absolute text-3xl left-[55%] top-[10%]">
               {props.desc}
               <div className="relative">
-                <DropDown urls={props.allURL.urls} />
+               
               </div>
             </div>
           </div>
